e1cb80917051bfa018210b7b1e24b0ab5f4b66be
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-01-19 15:04:19 -0500

    completed tasks stretch tasks.

--- index.js
+++ index.js
@@ -39,11 +39,14 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
-
-
+Person.prototype.eat = function(food) {if(this.stomach.length<10){this.stomach.push(food)}};
+Person.prototype.poop = function() {this.stomach = []};
+Person.prototype.toString = function() {return `${this.name}, ${this.age}`};
 
 
 
@@ -63,8 +66,20 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model,milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this. odometer = 0;
+}
+
+Car.prototype.fill = function(gallons){this.tank += gallons;}
+Car.prototype.drive = function(distance){
+  this.odometer += distance; 
+  this.tank -= (distance/this.milesPerGallon)
+  if(tank<= 0){
+    return `I ran out of fuel at ${odometer} miles!`;
+  }
 }
 
 
@@ -75,18 +90,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name,age,favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. If this is window binded(None of the other rules are applicable) it will return the window object or an error if it is in strict mode;
+  2. this can be bound implicitly. This means that when you have an object or method, this will be bound to what is left of the dot.
+  3. this can also be explicitly bound. This means that when using the functions call,apply, or bind, you can force a function to bind to a specific object for this.
+  4. lastly this can be bound via constructor functions by defining this, i.e. this.name =name;
 */
 
 
